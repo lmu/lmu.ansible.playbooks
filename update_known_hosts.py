673c8d95c34672c00a33295ffe2e8cce9308d30e
Alexander Loechel <Alexander.Loechel@lmu.de>
2015-12-11 09:59:41 +0100

    helper script

---
+++ update_known_hosts.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import subprocess
+import StringIO
+
+
+if __name__ == '__main__':
+
+    hosts = subprocess.check_output(['ansible', 'all', '--list-hosts'])
+
+    for host in hosts.split():
+        try:
+            subprocess.check_call(['ssh-keyscan', '-H', host], stdout=None, stderr=None)
+        except subprocess.CalledProcessError as e:
+            print(e)
+        except:
+            #import ipdb; ipdb.set_trace()
+            pass
