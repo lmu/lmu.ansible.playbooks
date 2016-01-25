657df2304a64abc0ae06bf6623d228781308d3e4
Alexander Loechel <Alexander.Loechel@lmu.de>
2016-01-25 14:35:23 +0100

    zrs replication for plone

--- update_known_hosts.py
+++ update_known_hosts.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+"""Helper Module for Ansible Playbooks to set known_hosts."""
+
 from __future__ import print_function
 
 import subprocess
@@ -6,14 +8,15 @@ import StringIO
 
 
 if __name__ == '__main__':
-
     hosts = subprocess.check_output(['ansible', 'all', '--list-hosts'])
 
     for host in hosts.split():
         try:
-            subprocess.check_call(['ssh-keyscan', '-H', host], stdout=None, stderr=None)
+            subprocess.check_call(['ssh-keyscan', '-H', host],
+                                  stdout=None,
+                                  stderr=None)
         except subprocess.CalledProcessError as e:
             print(e)
         except:
-            #import ipdb; ipdb.set_trace()
+            # import ipdb; ipdb.set_trace()
             pass
