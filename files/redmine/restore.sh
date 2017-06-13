546aa04bb19b5d32466ef6dfdbc1ab9145ca96a7
Alexander Loechel <Alexander.Loechel@lmu.de>
2017-06-13 19:41:36 +0200

    fixes

--- files/redmine/restore.sh
+++ files/redmine/restore.sh
@@ -1,19 +1,32 @@
 #!/usr/bin/env bash
 export DATE="2017-06-12"
 
-echo "Restore Redmine Instance Dez VI"
+echo "Restore Redmine Instance Dez VI - IT"
 sudo -u postgres dropdb --if-exists -U postgres redmine_dez_vi
 sudo -u postgres createdb -O redmine -U postgres -E UTF-8 redmine_dez_vi
+sudo rm -r /data/redmine/dez_vi/*
 
 sudo tar --gzip --extract --directory=/data/redmine/dez_vi --file=/home/ansible/backup/${DATE}_redmine_ref-vi.4_files.tar.gz      
 gunzip -c /home/ansible/backup/${DATE}_redmine_ref-vi.4_db.sql.gz | sudo -u postgres psql redmine_dez_vi
 
+
 echo "Restore Redmine Instance Webprojekte (Ref. VI.5)"
 sudo -u postgres dropdb --if-exists -U postgres redmine_vi5_webprojekte
 sudo -u postgres createdb -O redmine -U postgres -E UTF-8 redmine_vi5_webprojekte
+sudo rm -r /data/redmine/webprojekte/*
 
 sudo tar --gzip --extract --directory=/data/redmine/webprojekte --file=/home/ansible/backup/${DATE}_redmine_ref-vi.5_files.tar.gz    
 gunzip -c /home/ansible/backup/${DATE}_redmine_ref-vi.5_db.sql.gz | sudo -u postgres psql redmine_vi5_webprojekte
+
+
+echo "Restore Redmine Instance Dez VII - Haushalt"
+sudo -u postgres dropdb --if-exists -U postgres redmine_dez_vii
+sudo -u postgres createdb -O redmine -U postgres -E UTF-8 redmine_dez_vii
+sudo rm -r /data/redmine/dez_vi/*
+
+sudo tar --gzip --extract --directory=/data/redmine/dez_vii --file=/home/ansible/backup/${DATE}_redmine_dez-vii_files.tar.gz      
+gunzip -c /home/ansible/backup/${DATE}_redmine_dez-vii_db.sql.gz | sudo -u postgres psql redmine_dez_vii
+
 
 sudo chown -R redmine:redmine /data/redmine/
 echo "Restore Complete"
---
+++ files/ssl_gen.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+import argparse
+import subprocess
+import sys
+
+
+def gen_new_key_req_pair(hostname, domain='verwaltung.uni-muenchen.de', key_length=4096, ou='VI', days=365):
+    print("Generate Private Key and Request")
+    subprocess.call([
+        'openssl',
+        'req',
+        '-new',
+        '-newkey',
+        'rsa:'+str(key_length),
+        '-subj',
+        '/C=DE/ST=Bayern/L=Muenchen/O=Ludwig-Maximilians-Universitaet Muenchen/OU=' + ou + '/CN=' + hostname + domain,
+        '-nodes',
+        '-days',
+        str(days),
+        '-keyout',
+        'server-ssl/key/' + hostname + domain + '_key.pem',
+        '-out',
+        'server-ssl/req/' + hostname + domain + '_req.pem'
+    ])
+    print("key generated")
+    # print("generate intermediate crt")
+    # subprocess.call([
+    #     'openssl',
+    #     'req',
+    #     '-x509',
+    #     '-key', 
+    #     'server-ssl/key/' + hostname + domain + '_key.pem',
+    #     '-out',
+    #     'server-ssl/crt/' + hostname + domain + '_crt.pem'
+    # ])
+
+
+def gen_new_req_from_key(hostname, domain='verwaltung.uni-muenchen.de', key_length=4096, ou='VI'):
+    pass
+
+
+def show_key(arg):
+    pass
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Interenes Script um einfacher Keys und Key-Signing-Requests für SSL Zetrifikate zu erzeugen.")
+    parser.add_argument('hostname', type=str, help='Hostname / DNS-Name for which this Request is.')
+    parser.add_argument('--domain', type=str, help='Domain of the Host (default: ".verwaltung.uni-muenchen.de")', required=False, default='.verwaltung.uni-muenchen.de')
+    parser.add_argument('--ou', type=str, help='Organisation Unit (default: "VI")', required=False, default='VI')
+    parser.add_argument('--key_length', type=int, help='Key Length for RSA-Key (default: 4092)', required=False, default=4092)
+    parser.add_argument('--days', type=int, help='Days for Cert', required=False, default=365)
+
+    parser.add_argument('--show')
+
+    args = parser.parse_args()
+    print(args.__dict__)
+    if args.show:
+        show_key(args.hostname)
+    else:
+        gen_new_key_req_pair(args.hostname, domain=args.domain, key_length=args.key_length, ou=args.ou, days=args.days)
