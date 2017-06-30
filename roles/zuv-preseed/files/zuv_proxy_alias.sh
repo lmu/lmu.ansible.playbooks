8de83d2dbcd66bb492877fdf6e873ae7848a2292
Alexander Loechel <Alexander.Loechel@lmu.de>
2017-06-30 16:44:56 +0200

    new proxy

--- roles/zuv-preseed/files/zuv_proxy_alias.sh
+++ roles/zuv-preseed/files/zuv_proxy_alias.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 alias pip="pip --cert /etc/ssl/certs/lmu-proxy-CA.pem"
 alias pip="pip --cert /etc/ssl/certs/Verwaltungsnetz-Proxy-PEM.pem"
+alias pip="pip --cert /etc/ssl/certs/Verwaltungsnetz-Proxy-CA-NG.pem"
